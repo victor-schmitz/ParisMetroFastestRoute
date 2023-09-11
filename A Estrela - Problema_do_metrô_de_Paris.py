2351cdea7e234eea93da68b5ed6f8deb398773d1
victor-schmitz <victorcavalcan7i@gmail.com>
2023-09-11 18:45:50 -0300

    Atualização da lógica do algoritmo, adaptando ao projeto

--- A Estrela - Problema_do_metrô_de_Paris.py
+++ A Estrela - Problema_do_metrô_de_Paris.py
@@ -1,78 +1,85 @@
-def f(g, h, n): # Função f(n) = g(n) + h(n)
-    return g[n] + h[n] # O custo do caminho para o nó n é o custo do caminho para o nó n + o custo do caminho para o nó objetivo
+import heapq
+import math
 
-def update(remover, adicionar, m): # Função para atualizar a lista de nós explorados e a lista de nós a serem explorados
-    remover.remove(m) # Remove o nó m da lista de nós a serem explorados
-    adicionar.append(m) # Adiciona o nó m à lista de nós explorados
+# Example subway station distances
+station_distances = {
+    'E1': {'E2': 10, 'E3': 18.5, 'E4': 24.8, 'E5': 36.4, 'E6': 38.8, 'E7': 35.8, 'E8': 25.4, 'E9': 17.6, 'E10': 9.1, 'E11': 16.7, 'E12': 27.3, 'E13': 27.6, 'E14': 29.8},
+    'E2': {'E1': 10, 'E3': 8.5, 'E4': 14.8, 'E5': 26.6, 'E6': 29.1, 'E7': 26.1, 'E8': 17.3, 'E9': 10, 'E10': 3.5, 'E11': 15.5, 'E12': 20.9, 'E13': 19.1, 'E14': 21.8},
+    'E3': {'E1': 18.5, 'E2': 8.5, 'E4': 6.3, 'E5': 18.2, 'E6': 20.6, 'E7': 17.6, 'E8': 13.6, 'E9': 9.4, 'E10': 10.3, 'E11': 19.5, 'E12': 19.1, 'E13': 12.1, 'E14': 16.6},
+    'E4': {'E1': 24.8, 'E2': 14.8, 'E3': 6.3, 'E5': 12, 'E6': 14.4, 'E7': 11.5, 'E8': 12.4, 'E9': 12.6, 'E10': 16.7, 'E11': 23.6, 'E12': 18.6, 'E13': 10.6, 'E14': 15.4},
+    'E5': {'E1': 36.4, 'E2': 26.6, 'E3': 18.2, 'E4': 12, 'E6': 3, 'E7': 2.4, 'E8': 19.4, 'E9': 23.3, 'E10': 28.2, 'E11': 34.2, 'E12': 24.8, 'E13': 14.2, 'E14': 17.9},
+    'E6': {'E1': 38.8, 'E2': 29.1, 'E3': 20.6, 'E4': 14.4, 'E5': 3, 'E7': 3.3, 'E8': 22.3, 'E9': 25.7, 'E10': 30.3, 'E11': 36.7, 'E12': 27.6, 'E13': 15.2, 'E14': 18.2},
+    'E7': {'E1': 35.8, 'E2': 26.1, 'E3': 17.6, 'E4': 11.5, 'E5': 2.4, 'E6': 3.3, 'E8': 20, 'E9': 23, 'E10': 27.3, 'E11': 34.2, 'E12': 25.7, 'E13': 12.4, 'E14': 15.6},
+    'E8': {'E1': 25.4, 'E2': 17.3, 'E3': 13.6, 'E4': 12.4, 'E5': 19.4, 'E6': 22.3, 'E7': 20, 'E9': 8.2, 'E10': 20.3, 'E11': 16.1, 'E12': 6.4, 'E13': 22.7, 'E14': 27.6},
+    'E9': {'E1': 17.6, 'E2': 10, 'E3': 9.4, 'E4': 12.6, 'E5': 23.3, 'E6': 25.7, 'E7': 23, 'E8': 8.2, 'E10': 13.5, 'E11': 11.2, 'E12': 10.9, 'E13': 21.2, 'E14': 26.6},
+    'E10': {'E1': 9.1, 'E2': 3.5, 'E3': 10.3, 'E4': 16.7, 'E5': 28.2, 'E6': 30.3, 'E7': 27.3, 'E8': 20.3, 'E9': 13.5, 'E11': 17.6, 'E12': 24.2, 'E13': 18.7, 'E14': 21.2},
+    'E11': {'E1': 16.7, 'E2': 15.5, 'E3': 19.5, 'E4': 23.6, 'E5': 34.2, 'E6': 36.7, 'E7': 34.2, 'E8': 16.1, 'E9': 11.2, 'E10': 17.6, 'E12': 14.2, 'E13': 31.5, 'E14': 35.5},
+    'E12': {'E1': 27.3, 'E2': 20.9, 'E3': 19.1, 'E4': 18.6, 'E5': 24.8, 'E6': 27.6, 'E7': 25.7, 'E8': 6.4, 'E9': 10.9, 'E10': 24.2, 'E11': 14.2, 'E13': 28.8, 'E14': 33.6},
+    'E13': {'E1': 27.6, 'E2': 19.1, 'E3': 12.1, 'E4': 10.6, 'E5': 14.2, 'E6': 15.2, 'E7': 12.4, 'E8': 22.7, 'E9': 21.2, 'E10': 18.7, 'E11': 31.5, 'E12': 28.8, 'E14': 5.1},
+    'E14': {'E1': 29.8, 'E2': 21.8, 'E3': 16.6, 'E4': 15.4, 'E5': 17.9, 'E6': 18.2, 'E7': 15.6, 'E8': 27.6, 'E9': 26.6, 'E10': 21.2, 'E11': 35.5, 'E12': 33.6, 'E13': 5.1}
+}
 
-def a_estrela(custo, heurística, partida, objetos): # Função para encontrar o caminho mais curto entre o nó inicial e um dos nós objetivo
-    listaCaminhos = [] # Lista de caminhos
+def a_estrela(graph, start, goal):
+    open_set = []
+    heapq.heappush(open_set, (0, start))
+    came_from = {}
+    g_score = {node: float('inf') for node in graph}
+    g_score[start] = 0
+    f_score = {node: float('inf') for node in graph}
+    f_score[start] = heuristic(start, goal)
 
-    listaAberta = [partida] # Lista de nós a serem explorados
-    listaFechada = [] # Lista de nós já explorados
-
-    caminhoLen = {} # Dicionário para armazenar o custo do caminho
-    caminhoLen[partida] = 0 # O custo do caminho para o nó inicial é 0
-
-    paisNodes = {} # Dicionário para armazenar os nós pais
-    paisNodes[partida] = partida # O nó inicial não tem pai
-
-    while len(listaAberta) > 0: # Enquanto o openList não for vazio
-        node = None # Inicializa o nó atual como None
-
-        for n in listaAberta: # Para cada nó na openList
-            if node == None or f(caminhoLen, heurística, n) < f(caminhoLen, heurística, node): # Se o nó atual for None ou o custo do caminho para o nó atual for menor que o custo do caminho para o nó atual
-                node = n
-
-        if node == None: # Se o nó atual for None, não há caminho possível
-            break
-
-        if node in objetos: # Se o nó atual for um dos nós objetivo
-            fv_n = f(caminhoLen, heurística, node) # O custo do caminho para o nó atual é o custo do caminho para o nó atual + o custo do caminho para o nó objetivo. 
-            reconstruir = [] # Lista para reconstruir o caminho
-
-            aux = node # Inicializa o nó auxiliar como o nó atual
-            while paisNodes[aux] != aux: # Enquanto o nó auxiliar não for o nó inicial 
-                reconstruir.append(aux) # Adiciona o nó auxiliar à lista de nós para reconstruir o caminho
-                aux = paisNodes[aux] # O nó auxiliar passa a ser o nó pai do nó auxiliar
-
-            reconstruir.append(aux) # Adiciona o nó atual à lista de nós para reconstruir o caminho
-            reconstruir.reverse() # Inverte a lista de nós para obter o caminho
-
-            listaCaminhos.append((reconstruir, fv_n)) # Adiciona o caminho à lista de caminhos           
-
-            update(listaAberta, listaFechada, node) # Remove o nó atual da openList e adiciona à closedList 
-            continue
-
-
-        caminhoCusto = custo[node] # O custo do caminho para o nó atual é o custo do caminho para o nó atual    
-
-        for index in range(0, len(caminhoCusto)): # Para cada nó no grafo
-            peso = caminhoCusto[index] # O peso é o custo do caminho para o nó atual
-
-            if peso > 0: # Se o peso for maior que 0
-                if index not in listaAberta and index not in listaFechada: # Se o nó atual não estiver na openList e não estiver na closedList 
-                    listaAberta.append(index) # Adiciona o nó atual à openList
-                    paisNodes[index] = node # O nó pai do nó atual é o nó atual
-                    caminhoLen[index] = caminhoLen[node] + peso # O custo do caminho para o nó atual é o custo do caminho para o nó atual + o peso
-                else:
-                    if caminhoLen[index] > caminhoLen[node] + peso: # Se o custo do caminho para o nó atual for maior que o custo do caminho para o nó atual + o peso
-                        caminhoLen[index] = caminhoLen[node] + peso # O custo do caminho para o nó atual é o custo do caminho para o nó atual + o peso
-                        paisNodes[index] = node # O nó pai do nó atual é o nó atual
-
-                        if index in listaFechada: # Se o nó atual estiver na closedList
-                            update(listaFechada, listaAberta, index) # Remove o nó atual da closedList e adiciona à openList
+    while open_set:
+        _, current = heapq.heappop(open_set)
+        if current == goal:
+            return reconstruct_path(came_from, current)
         
-        update(listaAberta, listaFechada, node) # Remove o nó atual da openList e adiciona à closedList
+        for neighbor in graph[current]:
+            tentative_g_score = g_score[current] + graph[current][neighbor]
+            if tentative_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
+                heapq.heappush(open_set, (f_score[neighbor], neighbor))
+    
+    return None
 
-        if len(listaCaminhos) > 0: # Se a lista de caminhos não for vazia
-            listaCaminhos = sorted(listaCaminhos, key=lambda x: x[1]) # Ordena a lista de caminhos pelo custo do caminho
-            caminho = listaCaminhos[0][0] # O caminho é o primeiro caminho da lista de caminhos
-        else:
-            caminho = [] # O caminho é vazio
+def heuristic(node, goal):
+    if goal in station_distances[node]:
+        return station_distances[node][goal]
+    else:
+        return float('inf')  # Return a very high value if no direct connection exists
 
-    return caminho 
+def reconstruct_path(came_from, current):
+    path = [current]
+    while current in came_from:
+        current = came_from[current]
+        path.append(current)
+    path.reverse()
+    return path
 
+# Example subway network represented as an adjacency dictionary
+subway_network = {
+    'E1': {'E2': 10},
+    'E2': {'E1': 10, 'E3': 8.5, 'E9': 10, 'E10': 3.5},
+    'E3': {'E2': 8.5, 'E4': 6.3, 'E9': 9.4, 'E13': 18.7},
+    'E4': {'E3': 6.3, 'E5': 13, 'E8': 15.3, 'E13': 12.8},
+    'E5': {'E4': 13, 'E6': 3, 'E7': 2.4, 'E8': 30},
+    'E6': {'E5': 3},
+    'E7': {'E5': 2.4},
+    'E8': {'E4': 15.3, 'E9': 9.6, 'E12': 6.4, 'E5': 30},
+    'E9': {'E2': 10, 'E3': 9.4,'E11': 12.2, 'E8': 9.6},
+    'E10': {'E2': 3.5},
+    'E11': {'E9': 12.2},
+    'E12': {'E8': 6.4, 'E10': 3.5},
+    'E13': {'E14': 5.1, 'E3': 18.7, 'E4': 12.8},
+    'E14': {'E13': 5.1}    
+}
 
-                 
+start_station = 'E3'
+goal_station = 'E8'
 
+path = a_estrela(subway_network, start_station, goal_station)
+if path:
+    print("Shortest path:", path)
+else:
+    print("No path found.")
