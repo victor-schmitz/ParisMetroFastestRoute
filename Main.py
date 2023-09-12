772dce36b9c2bfa3e748a436382b0eb1bcc8e12a
victor-schmitz <victorcavalcan7i@gmail.com>
2023-09-11 23:50:54 -0300

    Sinalizações funcionais

--- Main.py
+++ Main.py
@@ -7,10 +7,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, estacoes_conectadas):
         super().__init__()
         self.setupUi(self)
-        
-        self.estacoes_conectadas = estacoes_conectadas  # Armazene as estações conectadas
 
-        # Conecte o botão Pesquisar Rota à função de pesquisa
+        self.estacoes_conectadas = estacoes_conectadas
+        self.estacao_labels = {
+            'E1': self.labelE1,
+            'E2': self.labelE2,
+            'E3': self.labelE3,
+            'E4': self.labelE4,
+            'E5': self.labelE5,
+            'E6': self.labelE6,
+            'E7': self.labelE7,
+            'E8': self.labelE8,
+            'E9': self.labelE9,
+            'E10': self.labelE10,
+            'E11': self.labelE11,
+            'E12': self.labelE12,
+            'E13': self.labelE13,
+            'E14': self.labelE14
+        }
+
         self.pushButton.clicked.connect(self.pesquisarRota)
 
     def pesquisarRota(self):
@@ -22,14 +37,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         rota_paris = RotaMetroParis()
 
         # Chame a função a_estrela para encontrar o caminho mais curto
-        caminho = rota_paris.a_estrela(self.estacoes_conectadas, estacao_origem, estacao_destino)
+        caminho = rota_paris.a_estrela(self.estacoes_conectadas, estacao_origem, estacao_destino)            
 
         # Verifique se um caminho foi encontrado
         if caminho:
             print("Caminho mais curto:", caminho)
+            self.mostrarCaminhoNoMapa(caminho)  # Chame a função para mostrar os labels
+
             # Exiba o caminho na interface ou faça o que quiser com ele
         else:
             QMessageBox.warning(self, "Sem rota encontrada", "Não foi possível encontrar uma rota entre as estações selecionadas.", QMessageBox.Ok)
+
+    def mostrarCaminhoNoMapa(self, caminho):
+        # Primeiro, oculte todas as labels de estação
+        for label in self.estacao_labels.values():
+            label.setHidden(True)
+            
+        # Em seguida, mostre apenas as labels das estações no caminho
+        for estacao in caminho:
+            if estacao in self.estacao_labels:
+                self.estacao_labels[estacao].setHidden(False)            
 
 def main():
     estacoes_conectadas = { # Estações conectadas
@@ -51,6 +78,38 @@ def main():
 
     app = QApplication(sys.argv)
     window = MainWindow(estacoes_conectadas)
+    window.labelE1.setHidden(True)  # Para ocultar o labelE1
+    window.labelE2.setHidden(True)  # Para ocultar o labelE2
+    window.labelE3.setHidden(True)  # Para ocultar o labelE3
+    window.labelE4.setHidden(True)  # Para ocultar o labelE4
+    window.labelE5.setHidden(True)  # Para ocultar o labelE5
+    window.labelE6.setHidden(True)  # Para ocultar o labelE6
+    window.labelE7.setHidden(True)  # Para ocultar o labelE7
+    window.labelE8.setHidden(True)  # Para ocultar o labelE8
+    window.labelE9.setHidden(True)  # Para ocultar o labelE9
+    window.labelE10.setHidden(True)  # Para ocultar o labelE10
+    window.labelE11.setHidden(True)  # Para ocultar o labelE11
+    window.labelE12.setHidden(True)  # Para ocultar o labelE12
+    window.labelE13.setHidden(True)  # Para ocultar o labelE13
+    window.labelE14.setHidden(True)  # Para ocultar o labelE14
+
+    estacao_labels = {
+    'E1': window.labelE1,
+    'E2': window.labelE2,
+    'E3': window.labelE3,
+    'E4': window.labelE4,
+    'E5': window.labelE5,
+    'E6': window.labelE6,
+    'E7': window.labelE7,
+    'E8': window.labelE8,
+    'E9': window.labelE9,
+    'E10': window.labelE10,
+    'E11': window.labelE11,
+    'E12': window.labelE12,
+    'E13': window.labelE13,
+    'E14': window.labelE14
+    }
+
     window.setFixedSize(523, 670)  # Defina o tamanho fixo da janela
     window.show()
     app.exec_()
